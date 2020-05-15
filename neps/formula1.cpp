c16ad41d8184ab224f39f74182787f9867a4614b
Artur Oscar <artur.junior@ufba.br>
2020-05-15 19:25:48 -0300

    Atualizações por Script

--- neps/formula1.cpp
+++ neps/formula1.cpp
@@ -6,27 +6,57 @@ using namespace std;
 #define endl '\n'
 
 struct piloto{
-	int id, pts, posicao[100];
+	int id, pts;
 };
-
+void zeraVetor(piloto v[], int p){
+	for(int i=0; i<p;i++){
+			v[i].id = i+1;
+			v[i].pts = 0;
+		}
+}
+int order(piloto a, piloto b){
+	if(a.pts != b.pts)
+		return a.pts > b.pts;
+	return a.id < b.id;
+}
 int main(){
 	desync;
-	int g,p;
+	int g,p,s,k, aux;
 	cin >> g >> p;
 
 	while(g != 0 && p != 0){
-		piloto vet[p];
-		int aux,contPos = 0;
-		for(int i =0;i<p;i++){
-			vet[i].pts = 0;
-			vet[i].id = i+1;
-		}
-		for(int i=0; i < g; i++){
-			for(int j=0; j< p; j++){
-				cin >> aux;
-				vet[i].posicao[contPos++]
+		int mat[g][p];
+		piloto pilotos[p];
+		zeraVetor(pilotos, p);
+		
+		for(int i=0; i < g; i++)
+			for(int j=0; j< p; j++)
+				cin >> mat[i][j];
 
+		cin >> s; //sistema de pontuacao
+		for(int i=0; i<s; i++){
+			cin >> k; // qtd pilotos no podio
+			zeraVetor(pilotos, p);
+			int pontos[p];
+			for(int j=0;j<p;j++) 
+				pontos[j] = 0;
+
+			for(int j=0; j<k;j++)
+				cin >> pontos[j]; //pontuacao ganha pelo j-esimo piloto
+
+			for(int j=0; j<p; j++)
+				for(int h =0; h< g; h++)
+					pilotos[j].pts += pontos[ (mat[h][j]-1) ];
+				
+			sort(pilotos, pilotos+p, order); 
+			
+			for(int z =0; z < p; z++){
+				cout << pilotos[z].id << " ";
+				if(z == p-1 || pilotos[z].pts > pilotos[z+1].pts){
+					break;
+				}
 			}
+			cout << endl;
 		}
 
 
